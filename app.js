c627a844d07e19b2a6ea99d549a32105a63368ec
Maitraye <maitraye.urmi@gmail.com>
2018-07-12 07:14:09 +0000

    show posts using for loop

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.get("/fallinlovewith/:thing", function(req, res){
     res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", function(req, res) {
+   var posts = [
+       {title: "post 1", author: "Abir"},
+       {title: "can you believe this", author: "Colt"},
+       {title: "My adorable pet", author: "Susy"}
+    ]; 
+    res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server is listening!");
 });
